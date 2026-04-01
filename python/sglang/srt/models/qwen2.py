965c2213d373a7da402b7dc37ee342fb670897cd
chenyue3 <chenyue3@sugon.com>
2026-04-01 10:25:55 +0800

    修改环境变量SGLANG_USE_FUSED_QWEN_RMS_ROTARY_EMBEDDING名称为SGLANG_USE_FUSED_RMS_ROTARY

--- python/sglang/srt/models/qwen2.py
+++ python/sglang/srt/models/qwen2.py
@@ -56,7 +56,7 @@ from sglang.srt.utils.hf_transformers_utils import get_rope_config
 _is_dcu = is_dcu()
 if _is_dcu:
     from lightop import split_qkv_rms_rotary_embedding_fuse_with_kv_store_quant
-_use_lightop_rotary_embedding_fuse=get_bool_env_var("SGLANG_USE_FUSED_QWEN_RMS_ROTARY_EMBEDDING")
+_use_fused_rms_rotary=get_bool_env_var("SGLANG_USE_FUSED_RMS_ROTARY")
 
 Qwen2Config = None
 
@@ -189,7 +189,7 @@ class Qwen2Attention(nn.Module):
     ) -> torch.Tensor:
         qkv, _ = self.qkv_proj(hidden_states)
         num_tokens = qkv.shape[0]
-        if _is_dcu and _use_lightop_rotary_embedding_fuse:
+        if _is_dcu and _use_fused_rms_rotary:
             cos_sin_cache = self.rotary_emb.cos_sin_cache
             if (cos_sin_cache.device != qkv.device
                     or cos_sin_cache.dtype != qkv.dtype):
