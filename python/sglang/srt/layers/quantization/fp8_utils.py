b8cd509f45f9a51adee404eb9104358653e869d1
chenyue3 <chenyue3@sugon.com>
2026-04-09 15:27:05 +0800

    [Feature]将hipblaslt_w8a8_block_fp8_linear中的per_token_group_quant_fp8量化极修改为lmslim的实现，以提升Qwen3.5-35B-A3B-FP8的性能

--- python/sglang/srt/layers/quantization/fp8_utils.py
+++ python/sglang/srt/layers/quantization/fp8_utils.py
@@ -49,7 +49,7 @@ from sglang.srt.utils import (
 )
 from lmslim import quant_ops
 from lmslim.quantize.quant_ops import BlockSize
-
+from lmslim.layers.gemm.fp8_utils import per_token_group_quant_fp8 as per_token_group_quant_fp8_dcu
 logger = logging.getLogger(__name__)
 
 _is_hip = is_hip()
@@ -821,7 +821,7 @@ def hipblaslt_w8a8_block_fp8_linear(
 ) -> torch.Tensor:
         input_2d = input.view(-1, input.shape[-1])
         output_shape = [*input.shape[:-1], weight.shape[0]]
-        q_input, input_scale = per_token_group_quant_fp8(
+        q_input, input_scale = per_token_group_quant_fp8_dcu(
             input_2d, block_size[1], column_major_scales=False
         )
         enum_block_size = BlockSize.block_128x128
