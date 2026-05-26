b38d1105ad0d7c77db91dce6672bf3482b30018d
tly123 <2442-tly123@users.noreply.192.168.54.209>
2026-05-26 19:29:14 +0800

    test: trigger dcu pr flow smoke

--- test/registered/dcu/interface/test_dcu_stage_b_flow_a.py
+++ test/registered/dcu/interface/test_dcu_stage_b_flow_a.py
@@ -8,6 +8,8 @@ register_dcu_ci(est_time=2, suite="stage-b-dcu")
 
 
 class TestDCUStageBFlowA(CustomTestCase):
+    """Flow-smoke test used to exercise DCU PR matrix routing."""
+
     def test_dcu_ci_env_marker(self):
         if os.environ.get("SGLANG_IS_IN_CI_DCU") != "1":
             self.skipTest("Not running inside DCU CI container; skipping marker check.")
